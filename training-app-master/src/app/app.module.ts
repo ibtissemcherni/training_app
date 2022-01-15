d13a758a5ebc8f8e1f11185b8d36be1752af57b1
ibtissem cherni <cherniibtissem08@gmail.com>
2022-01-15 11:43:04 +0100

    ibtissem

---
+++ training-app-master/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser'; import { NgModule } from '@angular/core'; 
+import { FormsModule } from '@angular/forms'; 
+import { AppComponent } from './app.component'; 
+import { SessionItemComponent } from './session-item/session-item.component'; 
+import { SessionItemListComponent } from './session-item-list/session-item-list.component'; 
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {RouterModule, Routes} from '@angular/router';
+import { InscriptionDisabledDirectiveDirective } from './inscription-disabled-directive.directive';
+
+const appRoutes: Routes = [ 
+    { 
+      path: 'list', component: SessionItemListComponent
+    }, 
+    { 
+      path: 'admin', loadChildren: () => import('./admin/admin.module').then(x =>x.AdminModule) 
+    }, 
+    { path: '', redirectTo: '/list', pathMatch: 'full' },  
+    { path: '**', component: PageNotFoundComponent } 
+]; 
+@NgModule({ 
+ declarations: [ 
+      AppComponent, 
+      SessionItemComponent, 
+      SessionItemListComponent, 
+      InscriptionDisabledDirectiveDirective, 
+      PageNotFoundComponent, PageNotFoundComponent, InscriptionDisabledDirectiveDirective, 
+ ], 
+ imports: [RouterModule.forRoot( 
+      appRoutes, 
+      { enableTracing: true } 
+      ), 
+      BrowserModule, 
+      FormsModule 
+ ], 
+ providers: [], 
+ bootstrap: [AppComponent] 
+}) 
+export class AppModule { } 
